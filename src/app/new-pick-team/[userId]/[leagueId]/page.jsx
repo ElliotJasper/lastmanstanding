ebb2b05ada820f245fb160e2192c37abc9143574
Elliot <elliotjasperis@gmail.com>
2024-11-06 20:58:58 +0000

    New conditional render to show league is inactive - Shows league is inactive - Creator of league has button to activate league

--- src/app/new-pick-team/[userId]/[leagueId]/page.jsx
+++ src/app/new-pick-team/[userId]/[leagueId]/page.jsx
@@ -225,7 +225,23 @@ export default function TeamSelectionPage({ params }) {
       </h1>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
         <div className="lg:col-span-2">
-          {winner ? (
+          {!leagueInfo.isactive ? (
+            <Card className="border-t-4 border-t-[#e01883]">
+              <CardHeader>
+                <CardTitle className="text-2xl text-[#4a82b0]">Inactive League</CardTitle>
+              </CardHeader>
+              <CardContent className="text-center">
+                <p className="text-lg mb-4">
+                  This league needs to be activated before you can start playing.
+                </p>
+                {leagueInfo.user_id === user.id && (
+                  <Button onClick={handleActivate} className="bg-[#4a82b0] hover:bg-[#4a82b0]/90">
+                    <PlayCircle className="mr-2 h-4 w-4" /> Activate League
+                  </Button>
+                )}
+              </CardContent>
+            </Card>
+          ) : winner ? (
             // Render the "winner" card
             <Card className="border-t-4 border-t-[#e01883]">
               <CardHeader>
