629234737bb79050fd0957d9ed956bbc0dba27eb
Elliot <elliotjasperis@gmail.com>
2024-11-27 23:04:29 +0000

    Add avatar url to returned users

--- src/app/api/get-league-users/[leagueId]/route.js
+++ src/app/api/get-league-users/[leagueId]/route.js
@@ -17,15 +17,20 @@ export async function GET(req, { params }) {
   // Fetch the user names based on the user IDs
   const userNames = await supabaseClient.getUserDisplayNames(userIds);
 
-  // Merge isEliminated field with the corresponding user details
-  const result = leagueUsers.map((leagueUser) => {
-    const userProfile = userNames.find((user) => user.user_id === leagueUser.user_id);
-    return {
-      user_id: leagueUser.user_id,
-      display_name: userProfile?.display_name || null,
-      isEliminated: leagueUser.isEliminated,
-    };
-  });
+  // Merge isEliminated field with the corresponding user details and avatar
+  const result = await Promise.all(
+    leagueUsers.map(async (leagueUser) => {
+      const userProfile = userNames.find((user) => user.user_id === leagueUser.user_id);
+      const avatarUrl = await supabaseClient.downloadAvatar(leagueUser.user_id);
+
+      return {
+        user_id: leagueUser.user_id,
+        display_name: userProfile?.display_name || null,
+        isEliminated: leagueUser.isEliminated,
+        avatar_url: avatarUrl, // Get avatar URL for each user
+      };
+    })
+  );
 
   for (let resultItem of result) {
     let userPicks = await supabaseClient.getUserPicks(resultItem.user_id, leagueId);
