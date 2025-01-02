cd07d12c49152936f90b69b30272a65639eac7b2
Elliot <elliotjasperis@gmail.com>
2025-01-02 20:05:45 +0000

    Navbar uses new auth context

--- src/components/custom/Navbar.jsx
+++ src/components/custom/Navbar.jsx
@@ -1,48 +1,12 @@
 "use client";
-
-import { useState, useEffect } from "react"; // Import useState and useEffect
-import { createClient } from "../../../utils/supabase/client";
-import Link from "next/link"; // Assuming you're using Next.js for routing
+import { useAuth } from "../../../contexts/AuthContext";
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 const Navbar = () => {
-  const [user, setUser] = useState(null); // State to hold the user
-  const [loading, setLoading] = useState(true); // State to track loading state
-
-  const handleLogout = async () => {
-    const supabase = createClient();
-    const { error } = await supabase.auth.signOut();
-    if (error) {
-      console.error("Error signing out:", error.message);
-    } else {
-      // Redirect to home or login page after logout
-      window.location.href = "/login";
-    }
-  };
-
-  useEffect(() => {
-    const supabase = createClient();
-
-    const checkAuth = async () => {
-      const {
-        data: { user },
-        error,
-      } = await supabase.auth.getUser();
-
-      if (user != null) {
-        setUser(user);
-      } else {
-        // If no user is found, redirect to login
-        window.location.href = "/login";
-      }
-      setLoading(false); // Set loading to false after checking authentication
-    };
-
-    checkAuth();
-  }, []);
+  const { user, loading, handleLogout } = useAuth();
 
   if (loading) {
-    // Optionally, you can add a loading indicator while checking auth
     return <div>Loading...</div>;
   }
 
@@ -54,7 +18,6 @@ const Navbar = () => {
         </Link>
         <div className="flex space-x-4">
           {user ? (
-            // If a user is logged in, show the profile link
             <>
               <Link href={`/profile/${user.id}`}>
                 <Button className="bg-[#4a82b0] hover:bg-[#3b74a2] text-white">Profile</Button>
@@ -64,7 +27,6 @@ const Navbar = () => {
               </Button>
             </>
           ) : (
-            // Otherwise, show the login and signup buttons
             <>
               <Link href="/login">
                 <Button variant="ghost" className="text-white hover:text-[#e01883] hover:bg-white/10">
