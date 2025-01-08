5def931cb7526ee02183973f1336fef7b6ad1bb8
Elliot <elliotjasperis@gmail.com>
2025-01-08 18:48:35 +0000

    Remove logs and old comments

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -40,10 +40,6 @@ export default function SignUpPage() {
 
     setIsLoading(true);
 
-    // Here you would typically send the signup data to your backend
-    console.log({ email, password });
-
-    // Simulate API call
     const { data, error } = await supabase.auth.signUp({
       email,
       password,
