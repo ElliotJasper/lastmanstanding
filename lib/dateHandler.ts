1b5f9957c52beaff64f1cfd4c01520bca120ac6e
Elliot <elliotjasperis@gmail.com>
2025-01-26 20:46:24 +0000

    Leagues cannot be activated sunday or monday

--- lib/dateHandler.ts
+++ lib/dateHandler.ts
@@ -76,6 +76,6 @@ export class DateHandler {
   static isWeekendPeriod(): boolean {
     const currentDay = new Date().getDay();
     // 5 = Friday, 6 = Saturday, 0 = Sunday, 1 = Monday
-    return currentDay === 1;
+    return currentDay === 1 || currentDay === 0;
   }
 }
